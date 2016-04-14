1f8938343fe40b7d3565efa47117a6d78dfe97fc
William Bautista <williambautista@gmail.com>
2016-04-14 14:20:01 +0100

    Returns Fizz, Buzz and FizzBuzz accordingly

--- src/test/com/fizzbuzz/FizzbuzzTest.java
+++ src/test/com/fizzbuzz/FizzbuzzTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import main.com.fizzbuzz.Fizzbuzz;
 import org.junit.Test;
+import org.junit.runner.notification.RunListener;
 
 public class FizzbuzzTest {
     private final Fizzbuzz fizzbuzz = new Fizzbuzz();
@@ -12,5 +13,20 @@ public class FizzbuzzTest {
     public void returnsNumber() throws Exception {
         assertEquals("1", fizzbuzz.fizzbuzzValue(1));
         assertEquals("2", fizzbuzz.fizzbuzzValue(2));
+    }
+
+    @Test
+    public void returnsFizzWhenNumberIsThree() throws Exception {
+        assertEquals("Fizz", fizzbuzz.fizzbuzzValue(3));
+    }
+
+    @Test
+    public void returnsBuzzWhenNumberIsFive() throws Exception {
+        assertEquals("Buzz", fizzbuzz.fizzbuzzValue(5));
+    }
+
+    @Test
+    public void returnsFizzBuzzWhenNumberIsDivisibleby15() throws Exception {
+        assertEquals("FizzBuzz", fizzbuzz.fizzbuzzValue(15));
     }
 }
