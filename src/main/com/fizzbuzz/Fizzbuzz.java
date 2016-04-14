103180459d80c3a1144a949e3b2eceeb3de82d13
William Bautista <williambautista@gmail.com>
2016-04-14 14:31:05 +0100

    FizzBuzz method refactored

--- src/main/com/fizzbuzz/Fizzbuzz.java
+++ src/main/com/fizzbuzz/Fizzbuzz.java
@@ -2,14 +2,9 @@ package main.com.fizzbuzz;
 
 public class Fizzbuzz {
     public String fizzbuzzValue(int number) {
-        if ((number % 15) == 0) {
-            return "FizzBuzz";
-        } else if ((number % 5) == 0) {
-            return "Buzz";
-        } else if ((number % 3) == 0) {
-            return  "Fizz";
-        }
-        return String.valueOf(number);
+        if ((number % 15) == 0) return "FizzBuzz";
+        if ((number % 5) == 0) return "Buzz";
+        return (number % 3) == 0 ? "Fizz" : String.valueOf(number);
     }
 }
 
