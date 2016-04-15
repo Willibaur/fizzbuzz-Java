8f9d0a3728f7b0de2cbb4d707afd72efb6014f8f
William Bautista <williambautista@gmail.com>
2016-04-15 08:58:19 +0100

    Naming test conventions implemented

--- src/test/com/fizzbuzz/FizzbuzzTest.java
+++ src/test/com/fizzbuzz/FizzbuzzTest.java
@@ -1,32 +1,31 @@
 package test.com.fizzbuzz;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 import main.com.fizzbuzz.Fizzbuzz;
-import org.junit.Test;
-import org.junit.runner.notification.RunListener;
 
 public class FizzbuzzTest {
-    private final Fizzbuzz fizzbuzz = new Fizzbuzz();
+    private Fizzbuzz fizzbuzz = new Fizzbuzz();
 
     @Test
-    public void returnsNumber() throws Exception {
+    public void whenNumberIsNotDivisibleBy3Or5ShouldReturnNumber() throws Exception {
         assertEquals("1", fizzbuzz.fizzbuzzValue(1));
         assertEquals("2", fizzbuzz.fizzbuzzValue(2));
     }
 
     @Test
-    public void returnsFizzWhenNumberIsThree() throws Exception {
+    public void whenNumberIsDivisibleBy3ShouldReturnFizz() throws Exception {
         assertEquals("Fizz", fizzbuzz.fizzbuzzValue(3));
     }
 
     @Test
-    public void returnsBuzzWhenNumberIsFive() throws Exception {
+    public void whenNumberIsDivisibleBy5ShouldReturnBuzz() throws Exception {
         assertEquals("Buzz", fizzbuzz.fizzbuzzValue(5));
     }
 
     @Test
-    public void returnsFizzBuzzWhenNumberIsDivisibleby15() throws Exception {
+    public void whenNumberIsDivisibleby15ShouldReturnFizzBuzz() throws Exception {
         assertEquals("FizzBuzz", fizzbuzz.fizzbuzzValue(15));
     }
 }
